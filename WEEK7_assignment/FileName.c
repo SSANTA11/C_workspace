c05b8f5cde0dcfd7436103fa40d84f20ad639773
SSANTA11 <ssanta011205@skuniv.ac.kr>
2024-10-16 00:23:18 +0900

    10월 16일 c언어 수업 과제 마무리

--- WEEK7_assignment/FileName.c
+++ WEEK7_assignment/FileName.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 
 int main() {
-	int num, input_1, count=0;
-	char ch, input_2;
+	int num, input;
+
 	printf("1.숫자2.소문자 선택: ");
 	scanf("%d", &num);
-	if (num == 1)
-		for (int i = 1; i <= 9; i++) {
+	if (num == 1){
+		for (int i = 1; i <= 9; i++)
 			printf("%d번째 %d\n", i + 1, i);
 			do {
 				printf("몇 번째 숫자를 출력할까요?(1~10):");
-				scanf("%d", &input_1);
-			} while (1 > input_1 || input_1 > 9);
-			printf("%d번째 숫자는 %d", input_1, input_2 - 1);
+				scanf("%d", &input);
+			} while (1 > input || input > 9);
+			printf("%d번째 숫자는 %d", input, input - 1);
 		}
-	else if (num==2)
-		for (int i = 'a'; i <= 'z'; i++) {
-			printf("%d번째 %d\n", count, i);
-			count += 1;
+	else if (num==2){
+		for (int i = 1; i <= 26; i++)
+			printf("%d번째 %c\n", i, i-1+'a');
 			do {
-
-			}
+				printf("몇 번째 문자를 출력할까요?(1~26):");
+				scanf("%d", &input);
+			} while (1 > input || input > 26);
+			printf("%d번째 문자는 %c", input, input - 1 + 'a');
 		}
 
 
