9b5415a87ebf0aad38fb6ddc1dbbae2809a82774
SSANTA11 <ssanta011205@skuniv.ac.kr>
2024-11-10 23:44:12 +0900

    11월 10일 학습

--- baeckjoon/practice.c
+++ baeckjoon/practice.c
@@ -11,32 +11,34 @@
 //    printf("%d\n",a%b);
 //}
 
-//백준 온라인 약수들의 합 9506번
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+////백준 온라인 약수들의 합 9506번
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//	int num, count = 0;
+//	do {
+//		scanf("%d", &num);
+//
+//		for (int i = 1; i < num; i++) {
+//			if (num % i == 0)
+//				count += i;
+//		}
+//		if (count == num) {
+//			printf("%d =", num);
+//			for (int i = 1; i < num; i++) {
+//				if (num % i == 0)
+//					printf(" %d", i);
+//				if (i < num - 1 && num % (i + 1) == 0) // 약수 사이에만 '+' 출력
+//					printf(" +");
+//			}
+//			count = 0;
+//			printf("\n");
+//		}
+//		else if (num != -1)
+//			printf("%d is NOT perfect.\n", num);
+//		count = 0;
+//
+//	} while (num !=-1);
+//}
 
-int main() {
-	int num, count = 0;
-	do {
-		scanf("%d", &num);
-
-		for (int i = 1; i < num; i++) {
-			if (num % i == 0)
-				count += i;
-		}
-		if (count == num) {
-			printf("%d =", num);
-			for (int i = 1; i < num; i++) {
-				if (num % i == 0)
-					printf(" %d", i);
-				if (i < num - 1 && num % (i + 1) == 0) // 약수 사이에만 '+' 출력
-					printf(" +");
-			}
-			count = 0;
-			printf("\n");
-		}
-		else if (num != -1)
-			printf("%d is NOT perfect.\n", num);
-		count = 0;
-	} while (num !=-1);
-}
